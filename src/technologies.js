c12e1b8290026ed32c51be45df1dafe563a67e4b
Kyle <kyleyoung.9210@gmail.com>
2021-06-10 15:12:14 -0400

    created an object of list of technologies to help prevent mispellings for devicon icons

---
+++ src/technologies.js
@@ -0,0 +1,16 @@
+const technologies = {
+	react: 'react',
+	javascript: 'javascript_badge',
+	github: 'github_badge',
+	git: 'git',
+	nodejs: 'nodejs',
+	npm: 'npm',
+	bootstrap: 'bootstrap',
+	css: 'css3',
+	html: 'html5',
+	heroku: 'heroku',
+	mysql: 'mysql',
+	mongodb: 'mongodb',
+};
+
+export default technologies;
