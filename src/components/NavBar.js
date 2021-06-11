8ea2ae053237ad8fbf81c317ab0778661f1a7160
Kyle <kyleyoung.9210@gmail.com>
2021-06-11 13:54:54 -0400

    handling scroll to change the background color of navbar.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,34 @@
-import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export default function NavBar() {
+	const [navClass, setNavClass] = useState('');
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll);
+	});
+
+	const handleScroll = () => {
+		window.pageYOffset > 150
+			? setNavClass('navScroll px-3')
+			: setNavClass('px-3');
+	};
+
 	return (
-		<Navbar bg="dark" variant="dark" expand="lg" className="px-3">
-			<Navbar.Brand href="/">
-				KMY<small>dev</small>
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-				<Nav className="mr-auto">
-					<Nav.Link href="/home">Home</Nav.Link>
-					<Nav.Link href="/about">About</Nav.Link>
-					<Nav.Link href="/projects">Projects</Nav.Link>
-					<Nav.Link href="/resume">Resume</Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
+		<Navbar id="mainNav" variant="dark" expand="lg" className={navClass}>
+			<Container>
+				<Navbar.Brand href="/">
+					KMY<small>dev</small>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="kmyNav" />
+				<Navbar.Collapse id="kmyNav" className="justify-content-end">
+					<Nav className="mr-auto">
+						<Nav.Link href="/home">Home</Nav.Link>
+						<Nav.Link href="/about">About</Nav.Link>
+						<Nav.Link href="/projects">Projects</Nav.Link>
+						<Nav.Link href="/resume">Resume</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
 		</Navbar>
 	);
 }
