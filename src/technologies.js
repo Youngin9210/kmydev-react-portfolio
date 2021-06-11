5d406a09e9793ad9986388ea4cfb92f2ac019524
Kyle <kyleyoung.9210@gmail.com>
2021-06-11 13:57:59 -0400

    updated object for react-icons package

--- src/technologies.js
+++ src/technologies.js
@@ -1,16 +1,29 @@
+import { DiMongodb } from 'react-icons/di';
+import {
+	FaBootstrap,
+	FaCss3Alt,
+	FaGitAlt,
+	FaGithub,
+	FaHtml5,
+	FaJsSquare,
+	FaNodeJs,
+	FaNpm,
+	FaReact,
+} from 'react-icons/fa';
+import { GrHeroku, GrMysql } from 'react-icons/gr';
 const technologies = {
-	react: 'react',
-	javascript: 'javascript_badge',
-	github: 'github_badge',
-	git: 'git',
-	nodejs: 'nodejs',
-	npm: 'npm',
-	bootstrap: 'bootstrap',
-	css: 'css3',
-	html: 'html5',
-	heroku: 'heroku',
-	mysql: 'mysql',
-	mongodb: 'mongodb',
+	react: <FaReact />,
+	javascript: <FaJsSquare />,
+	github: <FaGithub />,
+	git: <FaGitAlt />,
+	nodejs: <FaNodeJs />,
+	npm: <FaNpm />,
+	bootstrap: <FaBootstrap />,
+	css: <FaCss3Alt />,
+	html: <FaHtml5 />,
+	heroku: <GrHeroku />,
+	mysql: <GrMysql />,
+	mongodb: <DiMongodb />,
 };
 
 export default technologies;
