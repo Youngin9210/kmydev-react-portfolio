d0f9f7dd7848e356ffc19980d17daa90de0fecac
Kyle <kyleyoung.9210@gmail.com>
2021-06-13 10:49:19 -0400

    making header responsive to page size and adding an svg instead of headshot

--- src/pages/Home/Header.js
+++ src/pages/Home/Header.js
@@ -6,19 +6,21 @@ export default function Header() {
 	return (
 		<Container
 			id="header"
-			className="text-white d-flex justify-content-between align-items-center"
+			className="text-white d-flex flex-column flex-lg-row justify-content-between align-items-center"
 		>
-			<Col md="6" className="header-content">
+			<Col md={8} lg={6} className="header-content">
 				<h2 id="name" className="display-3 mb-0">
 					Hello!
 				</h2>
 				<h2 id="name" className="display-3 mb-0">
-					Kyle Young
+					My name is <span>Kyle</span>!
 				</h2>
 				<h4 className="display-5">Full Stack Developer</h4>
 			</Col>
-			<Col md="6" className="header-svg">
-				<img src={headerImg} alt="" />
+			<Col md={8} lg={6} className="header-svg">
+				<div>
+					<img src={headerImg} alt="" />
+				</div>
 			</Col>
 		</Container>
 	);
