1eca57a088755f461df38a816775a717c1b0c19c
Kyle <kyleyoung.9210@gmail.com>
2021-06-13 13:38:54 -0400

    increasing size of icons and also adding some margin

--- src/technologies.js
+++ src/technologies.js
@@ -12,18 +12,18 @@ import {
 } from 'react-icons/fa';
 import { GrHeroku, GrMysql } from 'react-icons/gr';
 const technologies = {
-	react: <FaReact />,
-	javascript: <FaJsSquare />,
-	github: <FaGithub />,
-	git: <FaGitAlt />,
-	nodejs: <FaNodeJs />,
-	npm: <FaNpm />,
-	bootstrap: <FaBootstrap />,
-	css: <FaCss3Alt />,
-	html: <FaHtml5 />,
-	heroku: <GrHeroku />,
-	mysql: <GrMysql />,
-	mongodb: <DiMongodb />,
+	react: <FaReact size="32" className="m-1" />,
+	javascript: <FaJsSquare size="32" className="m-1" />,
+	github: <FaGithub size="32" className="m-1" />,
+	git: <FaGitAlt size="32" className="m-1" />,
+	nodejs: <FaNodeJs size="32" className="m-1" />,
+	npm: <FaNpm size="32" className="m-1" />,
+	bootstrap: <FaBootstrap size="32" className="m-1" />,
+	css: <FaCss3Alt size="32" className="m-1" />,
+	html: <FaHtml5 size="32" className="m-1" />,
+	heroku: <GrHeroku size="32" className="m-1" />,
+	mysql: <GrMysql size="32" className="m-1" />,
+	mongodb: <DiMongodb size="32" className="m-1" />,
 };
 
 export default technologies;
