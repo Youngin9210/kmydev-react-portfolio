1773a8c56f017fbfa4931948916e05e5559f9eb3
Kyle <kyleyoung.9210@gmail.com>
2021-06-15 18:03:39 -0400

    changed header image and styled new image

--- src/pages/Home/HeaderImage.js
+++ src/pages/Home/HeaderImage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
-import headerImg from './assets/images/header-img.png';
+import headshot from './assets/images/headshot.png';
 
 export default function HeaderImage() {
 	return (
 		<Col md={8} lg={6} className="header-img">
-			<img src={headerImg} alt="" />
+			<img src={headshot} alt="" className="headshot" />
 		</Col>
 	);
 }
