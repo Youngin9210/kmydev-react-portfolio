27fbfb4bcb6f9dd7d2aaff2e9a3061b880d7f126
Kyle <kyleyoung.9210@gmail.com>
2021-06-15 17:35:09 -0400

    making footer more responsive and adding links for phone, email, github, and linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Container, ListGroup } from 'react-bootstrap';
 import { FaGithubSquare, FaLinkedin, FaPhone } from 'react-icons/fa';
+import { SiGmail } from 'react-icons/si';
 import { Link } from 'react-router-dom';
 
 export default function NavBar() {
@@ -9,17 +10,29 @@ export default function NavBar() {
 		<Container
 			id="footer"
 			fluid
-			className="d-flex flex-wrap justify-content-between text-center py-2"
+			className="d-flex flex-wrap-reverse justify-content-between text-center py-2"
 		>
-			<Col md={4} className="d-flex align-items-center justify-content-center">
+			<Col
+				xs={12}
+				lg={4}
+				className="d-flex align-items-center justify-content-center py-1"
+			>
 				<a href="https://storyset.com/work" className="attribution">
 					Work illustrations by Storyset
 				</a>
 			</Col>
-			<Col md={4} className="d-flex align-items-center justify-content-center">
+			<Col
+				xs={12}
+				lg={4}
+				className="d-flex align-items-center justify-content-center py-1"
+			>
 				<h5>Copyright &copy; {year} KMYdev</h5>
 			</Col>
-			<Col md={4} className="d-flex align-items-center justify-content-center">
+			<Col
+				xs={12}
+				lg={4}
+				className="d-flex align-items-center justify-content-center py-1"
+			>
 				<ListGroup horizontal className="footerList">
 					<ListGroup.Item className="footerItem">
 						<Link
@@ -36,7 +49,26 @@ export default function NavBar() {
 					</ListGroup.Item>
 					<ListGroup.Item className="footerItem">
 						<Link className="footerLink" to="">
-							<FaPhone />
+							<FaPhone
+								to="#"
+								onClick={(e) => {
+									window.location = 'tel:+19376702584';
+									e.preventDefault();
+								}}
+							/>
+						</Link>
+					</ListGroup.Item>
+					<ListGroup.Item className="footerItem">
+						<Link
+							className="footerLink"
+							to="#"
+							onClick={(e) => {
+								window.location =
+									'mailto:kyleyoung.9210@gmail.com?subject=Portfolio';
+								e.preventDefault();
+							}}
+						>
+							<SiGmail />
 						</Link>
 					</ListGroup.Item>
 				</ListGroup>
