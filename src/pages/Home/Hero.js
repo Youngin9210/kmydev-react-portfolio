f9e83de36920a9245be14c6b8944a8eea7a03ad5
Kyle <kyleyoung.9210@gmail.com>
2021-06-15 19:38:50 -0400

    created a resume btn component for a downloadable resume

--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './Header';
+import ResumeBtn from './ResumeBtn';
 
 export default function Hero() {
 	return (
 		<Container
 			fluid
 			id="hero"
-			className="d-flex justify-content-center align-items-center"
+			className="d-flex justify-content-center align-items-center flex-column justify-content-evenly"
 		>
 			<div id="overlay"></div>
 			<Header />
+			<ResumeBtn />
 		</Container>
 	);
 }
