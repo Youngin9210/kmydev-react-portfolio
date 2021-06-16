1a9879973a7dd64a22b065a5fd054dda8122d4c6
Kyle <kyleyoung.9210@gmail.com>
2021-06-15 22:11:10 -0400

    changing routing. removing not-found and redirects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import {
-	BrowserRouter as Router,
-	Redirect,
-	Route,
-	Switch,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import About from './pages/About/index';
 import Home from './pages/Home/index';
-import NotFound from './pages/Not-Found/index';
 import Projects from './pages/Projects/index';
 
 const App = () => {
@@ -20,9 +14,6 @@ const App = () => {
 				<Route exact path="/home" component={Home}></Route>
 				<Route exact path="/about" component={About}></Route>
 				<Route exact path="/projects" component={Projects}></Route>
-				<Route path="/not-found" component={NotFound}></Route>
-				<Redirect from="/" exact to="/home" />
-				<Redirect to="/not-found" />
 			</Switch>
 			<Footer />
 		</Router>
