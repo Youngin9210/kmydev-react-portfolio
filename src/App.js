b76bea7665aca47ae65b57d784c99ada4b01a802
Kyle <kyleyoung.9210@gmail.com>
2021-06-16 09:37:14 -0400

    gh-pages troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import About from './pages/About/index';
@@ -8,15 +8,15 @@ import Projects from './pages/Projects/index';
 
 const App = () => {
 	return (
-		<Router>
+		<BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
 			<NavBar />
 			<Switch>
-				<Route exact path={['/', '/home']} component={Home}></Route>
+				<Route exact path="" component={Home}></Route>
 				<Route exact path="/about" component={About}></Route>
 				<Route exact path="/projects" component={Projects}></Route>
 			</Switch>
 			<Footer />
-		</Router>
+		</BrowserRouter>
 	);
 };
 
