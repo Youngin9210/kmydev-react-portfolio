11018334a999e2e69397c626a05f27b65e5a0553
Kyle <kyleyoung.9210@gmail.com>
2021-07-05 16:33:32 +0000

    Changing to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
-import About from './pages/About/index';
-import Home from './pages/Home/index';
-import Projects from './pages/Projects/index';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import About from "./pages/About/index";
+import Home from "./pages/Home/index";
+import Projects from "./pages/Projects/index";
 
 const App = () => {
-	return (
-		<Router>
-			<NavBar />
-			<Switch>
-				<Route exact path={['/', '/home']} component={Home}></Route>
-				<Route exact path="/about" component={About}></Route>
-				<Route exact path="/projects" component={Projects}></Route>
-			</Switch>
-			<Footer />
-		</Router>
-	);
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route exact path={["/", "/home"]} component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/projects" component={Projects}></Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
